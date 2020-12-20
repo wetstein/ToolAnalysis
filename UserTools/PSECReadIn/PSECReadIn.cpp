9e947dcc618cb3cf60d89cb59969ecb92d72e495
Matt Wetstein <wetstein@zh-a426-mbp.physics.iastate.edu>
2020-12-19 19:27:17 -0600

    fully working now

--- UserTools/PSECReadIn/PSECReadIn.cpp
+++ UserTools/PSECReadIn/PSECReadIn.cpp
@@ -16,7 +16,6 @@ bool PSECReadIn::Initialise(std::string configfile, DataModel &data){
   //get the data file name from the config file
   //needs to have a path to the data files (inside the "")
   m_variables.Get("PSECinputfile", NewFile);
-    
   //Opening data file
   DataFile.open(NewFile);
     
@@ -62,24 +61,36 @@ bool PSECReadIn::Execute(){
       unsigned long channelNo=0; //channel number
       istringstream iss(nextLine); //copies the current line in the file
       int location=-1; //counts the current perameter in the line
+      string stempValue; //current string in the line
       int tempValue; //current int in the line
+
       //starts the loop at the beginning of the line
-      while(iss >> tempValue)
+      while(iss >> stempValue)
       {
           location++;
-        //  cout<<location<<endl;
+          //  cout<<location<<endl;
           //when location==0, it is at the sample number
+          
+          //cout<<"beginning "<<stempValue<<endl;
+          
           if(location==0)
           {
-              sampleNo=tempValue;
+              sampleNo = stoi(stempValue, 0, 16);
+              
+              //(int)strtol(tempValue, NULL, 16);
+              //cout<<"SAMPLE: "<<sampleNo<<endl;
+              //sampleNo=tempValue;
               continue;
           }
           //this is the meta data per format
           if((location)%31==0)
           {
-             // cout<<"yes"<<endl;
+              //cout<<"yes"<<endl;
               continue;
           }
+          
+          int tempValue = stoi(stempValue, 0, 10);
+
           
           if(sampleNo==0)
           {
@@ -87,21 +98,24 @@ bool PSECReadIn::Execute(){
               //if none of the above if statements happen
               //it is going through each channel 0 through the NChannels-1
               //the tempValue will be placed into the tempwav
-              tempwav.PushSample(tempValue);
+              tempwav.PushSample((double)tempValue);
               //cout<<"does it get here? "<<location<<" "<<tempValue<<endl;
               vector<Waveform<double>> Vtempwav;
               Vtempwav.push_back(tempwav);
               //each tempwav will be inserted into the correct channelno
-              LAPPDWaveforms->insert(pair<unsigned long, vector<Waveform<double>>>(channelNo, Vtempwav));
+              LAPPDWaveforms->insert(pair<unsigned long, vector<Waveform<double>>>(channelNo,Vtempwav));
               //clear the tempwav for a new value
+              //cout<<"FIRST SAMPLE: "<<eventNo<<" "<<sampleNo<<" "<<channelNo<<" "<<tempValue<<endl;
           }
           else
           {
-              itr = LAPPDWaveforms->find(channelNo);
-              vector<Waveform<double>> Vtemp = itr->second;
-             // cout<<"Can I get here?"<<endl;
-              (Vtemp.at(0)).PushSample(tempValue);
-              LAPPDWaveforms->insert(pair<unsigned long, vector<Waveform<double>>>(channelNo, Vtemp));
+              //fitr = LAPPDWaveforms->find(channelNo);
+              (((LAPPDWaveforms->find(channelNo))->second).at(0)).PushSample((double)tempValue);
+              //cout<<"LATE SAMPLES: "<<eventNo<<" "<<sampleNo<<" "<<channelNo<<" "<<tempValue<<endl;
+              //cout<<"Nsamples "<<(((LAPPDWaveforms->find(channelNo))->second).at(0)).Samples().size()<<endl;
+              //vector<Waveform<double>> Vtemp = itr->second;
+              //((itr->second).at(0)).PushSample(tempValue);
+              //LAPPDWaveforms->insert(pair<unsigned long, vector<Waveform<double>>>(channelNo, Vtemp));
           }
         
         channelNo++;
@@ -109,15 +123,20 @@ bool PSECReadIn::Execute(){
       
       //when the sampleNo gets to ff
       //it is at sample 256 and needs to go to the next event
-      if(sampleNo=='ff')
+      //if(sampleNo=='ff')
+      if(sampleNo==255)
       {
+          cout<<"DID THIS HAPPEN "<<sampleNo<<" "<<eventNo<<endl;
           eventNo++;
           break;
       }
+      
+      //cout<<"at the end"<<endl;
   }
-  m_data->Stores["ANNIEEvent"]->Set("LAPPDWaveforms",LAPPDWaveforms);
-
     
+  m_data->Stores["ANNIEEvent"]->Set("LAPPDWaveforms",LAPPDWaveforms);
+  LAPPDWaveforms->clear();
+
   return true;
 }
 
